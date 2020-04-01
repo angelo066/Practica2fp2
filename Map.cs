55784cfdfd6d517c2dea158bc5b985809e06837e
Angelo06 <angelo06@ucm.es>
2020-04-01 18:58:02 +0200

    Intentando que kraken me deje usar git

---
+++ Map.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public enum Direction { North, South, East, West };
+class Map
+{
+    // items
+    public struct Item
+    {
+        public string name, description;
+        public int hp; // health points
+        public int weight; // peso del item
+    }
+    // lugares del mapa
+    public struct Room
+    {
+        public string name, description;
+        public bool exit; // es salida?
+        public int[] connections; // vector de 4 componentes
+                                  // con el lugar al norte, sur, este y oeste
+                                  // -1 si no hay conexion
+        public Lista itemsInRoom; // indices al vector de items n los items del lugar
+    }
+    Room[] rooms; // vector de lugares del mapa
+    Item[] items; // vector de items del juego
+    int nRooms, nItems; // numero de lugares y numero de items
+    int entryRoom; // numero de la habitacion de entrada (leida del mapa)
+    public Map(int numRooms, int numItems)
+    {
+        nRooms = numRooms;
+        nItems = numItems;
+        rooms = new Room[nRooms];
+        items = new Item[nItems];
+    }
+    public void ReadMap(string file)
+    {
+
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//Ángel López Benítez
+//David Rodríguez Gómez
+using System;
+
+namespace Practica2fp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
